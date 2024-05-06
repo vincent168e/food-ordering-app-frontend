ec8923a5631e260a5e0f4d04416c8fee5e85fcc8
Vincent Tsang <vincent168e@gmail.com>
2024-05-06 17:07:01 +0800

    Setup Auth0

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,4 +1,5 @@
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import {
   Sheet,
   SheetContent,
@@ -8,8 +9,11 @@ import {
 } from "./ui/sheet";
 import { Separator } from "@radix-ui/react-separator";
 import { Button } from "./ui/button";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -17,11 +21,27 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          <span>Welcome to JoyfulEats.com!</span>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to JoyfulEats.com!</span>
+          )}
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              className="flex-1 font-bold bg-orange-500"
+              onClick={async () => await loginWithRedirect()}
+            >
+              Log In
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
