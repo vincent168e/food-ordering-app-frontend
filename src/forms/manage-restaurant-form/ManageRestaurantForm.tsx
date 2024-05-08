b7250416da930f8b399d2faa6a697fb052e54edd
Vincent Tsang <vincent168e@gmail.com>
2024-05-08 19:39:33 +0800

    Api request to create restaurant

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -57,7 +57,33 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
   });
 
   const onSubmit = (formDataJson: RestaurantFormData) => {
-    // TODO: convert formDataJson to a new FormData object
+    const formData = new FormData();
+
+    formData.append("restaurantName", formDataJson.restaurantName);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    // Depend on a country's lowest currency unit
+    formData.append(
+      "deliveryPrice",
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
+    });
+    formData.append(`imageFile`, formDataJson.imageFile);
+
+    onSave(formData);
   };
 
   return (
